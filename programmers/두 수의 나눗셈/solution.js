449e655cc73fc3a1001eca23a05e028d789cc527
Gon1782 <116235243+Gon1782@users.noreply.github.com>
2022-11-09 20:48:18 +0900

    updated at 2022. 11. 9. 오후 8:48:14

--- programmers/두 수의 나눗셈/solution.js
+++ programmers/두 수의 나눗셈/solution.js
@@ -1,5 +1,5 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/120806?language=javascript
 
 function solution(num1, num2) {
-    return Math.trunc(num1 / num2 * 1000);
+    return Math.floor(num1 / num2 * 1000);
 }
