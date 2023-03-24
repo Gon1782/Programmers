bf78b6c8743aee5f61aa807e1351c2dce530688b
Gon <hss3522@gmail.com>
2023-03-25 00:45:05 +0900

    2023-03-25

---
+++ LV.1/연속 부분 수열 합의 개수.js
@@ -0,0 +1,12 @@
+function solution(elements) {
+  const double = [...elements, ...elements];
+  const set = new Set();
+  for (let i = 0; i < elements.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < elements.length; j++) {
+      sum += double[i + j];
+      set.add(sum);
+    }
+  }
+  return set.size;
+}
